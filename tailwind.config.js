3eee9b639fcf8146f863a900c43439c1d4f0dd93
Abrar Hasan Chowdhury <104994165+abrarhasanch@users.noreply.github.com>
2025-05-30 20:22:21 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: 'class', // Keep this for your dark mode toggle
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
